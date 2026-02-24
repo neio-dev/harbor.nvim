265cbe70a24cba269d2405fed4691c054def1e6d
neio <marcon.dylan.fr@gmail.com>
2026-02-24 10:22:10 +0100

    feat(types): add types meta file

---
+++ types.lua
@@ -0,0 +1,45 @@
+---@class Harbor
+---@field dock Dock
+---@field bay Bay
+---@field lighthouse Lighthouse
+---@field extensions {[string]: Extension}
+---@field active_ship? Ship|Empty
+
+---@class CursorPosition
+---@field col number
+---@field row number
+
+---@alias PossibleList "dock"|"bay"
+
+---@class Ship
+---@field value string
+---@field position CursorPosition 
+---@field current_list PossibleList 
+
+---@class Fleet
+---@field name string
+---@field ships {[string]: Ship}
+---@field resolve? RESOLVE.replace|RESOLVE.prepend
+---@field harbor Harbor
+---@field previous_index? number
+---@field length number
+---@field history_length number
+
+---@class Bay: Fleet
+
+---@class Dock: Fleet
+
+---@class SavedData
+---@field dock Ship[]
+---@field bay Ship[]
+
+---@class SessionManager
+---@field sessions {}
+---@field path string
+---@field dir string
+---@field harbor Harbor
+---@field current_session string
+
+---@class Lighthouse
+
+return {}
